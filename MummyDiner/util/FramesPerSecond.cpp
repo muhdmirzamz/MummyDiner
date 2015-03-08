2b76bec3d78bf3f1751ac19515c77ad640067bfd
muhdmirzamz <muhdmirzamz@gmail.com>
2015-03-08 22:26:52 +0800

    added smoke

---
+++ MummyDiner/util/FramesPerSecond.cpp
@@ -0,0 +1,9 @@
+//
+//  FramesPerSecond.cpp
+//  MummyDiner
+//
+//  Created by Muhd Mirza on 5/3/15.
+//  Copyright (c) 2015 Muhd Mirza. All rights reserved.
+//
+
+#include "FramesPerSecond.h"
---
+++ MummyDiner/util/FramesPerSecond.h
@@ -0,0 +1,14 @@
+//
+//  FramesPerSecond.h
+//  MummyDiner
+//
+//  Created by Muhd Mirza on 5/3/15.
+//  Copyright (c) 2015 Muhd Mirza. All rights reserved.
+//
+
+#ifndef __MummyDiner__FramesPerSecond__
+#define __MummyDiner__FramesPerSecond__
+
+#include <stdio.h>
+
+#endif /* defined(__MummyDiner__FramesPerSecond__) */
