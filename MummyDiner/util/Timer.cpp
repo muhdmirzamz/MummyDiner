80701377c8e65992d7a4f78e75f5c7033c25e396
muhdmirzamz <muhdmirzamz@gmail.com>
2015-03-15 22:00:23 +0800

    Added timer class

---
+++ MummyDiner/util/Timer.cpp
@@ -0,0 +1,9 @@
+//
+//  Timer.cpp
+//  MummyDiner
+//
+//  Created by Muhd Mirza on 12/3/15.
+//  Copyright (c) 2015 Muhd Mirza. All rights reserved.
+//
+
+#include "Timer.h"
---
+++ MummyDiner/util/Timer.h
@@ -0,0 +1,14 @@
+//
+//  Timer.h
+//  MummyDiner
+//
+//  Created by Muhd Mirza on 12/3/15.
+//  Copyright (c) 2015 Muhd Mirza. All rights reserved.
+//
+
+#ifndef __MummyDiner__Timer__
+#define __MummyDiner__Timer__
+
+#include <stdio.h>
+
+#endif /* defined(__MummyDiner__Timer__) */
