1c1e5deda188135cd6cb025e497b9d99b2d9a8a7
muhdmirzamz <muhdmirzamz@gmail.com>
2015-04-03 13:52:55 +0800

    File extension changes

---
+++ MummyDiner/gui/ToggleClass.cpp
@@ -0,0 +1,9 @@
+//
+//  ToggleClass.cpp
+//  MummyDiner
+//
+//  Created by Muhd Mirza on 31/3/15.
+//  Copyright (c) 2015 Muhd Mirza. All rights reserved.
+//
+
+#include "ToggleClass.h"
---
+++ MummyDiner/gui/ToggleClass.h
@@ -0,0 +1,14 @@
+//
+//  ToggleClass.h
+//  MummyDiner
+//
+//  Created by Muhd Mirza on 31/3/15.
+//  Copyright (c) 2015 Muhd Mirza. All rights reserved.
+//
+
+#ifndef __MummyDiner__ToggleClass__
+#define __MummyDiner__ToggleClass__
+
+#include <stdio.h>
+
+#endif /* defined(__MummyDiner__ToggleClass__) */
