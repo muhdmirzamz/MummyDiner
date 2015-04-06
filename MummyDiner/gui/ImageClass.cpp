a1e59c9364a0a644972f8b780714a4b0dd47a8ab
muhdmirzamz <muhdmirzamz@gmail.com>
2015-04-06 18:15:05 +0800

    Added new image class to facilitate rendering of GUIs in help screen

--- MummyDiner/gui/ImageClass.cpp
+++ MummyDiner/gui/ImageClass.cpp
@@ -7,3 +7,26 @@
 //
 
 #include "ImageClass.h"
+
+ImageClass::ImageClass() {
+	
+}
+
+void ImageClass::setImage(const char *file) {
+	_image.loadFromFile(file);
+	_texture.loadFromImage(_image);
+	_sprite.setTexture(_texture);
+	_sprite.setScale(0.1, 0.1);
+}
+
+void ImageClass::setScale(float x, float y) {
+	_sprite.setScale(x, y);
+}
+
+void ImageClass::setPosition(float x, float y) {
+	_sprite.setPosition(x, y);
+}
+
+void ImageClass::renderImage(RenderWindow &window) {
+	window.draw(_sprite);
+}
--- MummyDiner/gui/ImageClass.h
+++ MummyDiner/gui/ImageClass.h
@@ -11,4 +11,23 @@
 
 #include <stdio.h>
 
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class ImageClass {
+	public:
+		ImageClass();
+	
+		void setImage(const char *file);
+		void setScale(float x, float y);
+		void setPosition(float x, float y);
+		void renderImage(RenderWindow &window);
+	
+	private:
+		Image _image;
+		Texture _texture;
+		Sprite _sprite;
+};
+
 #endif /* defined(__MummyDiner__ImageClass__) */
